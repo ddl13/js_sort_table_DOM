6d1cffeb641f37f5599fd2c63886f0347fafdd18
Denys Lytvyn <ddl@lytvyn.com>
2024-03-16 13:14:18 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const tHead = document.querySelectorAll("th");
+for(let i = 0; i < tHead.length; i++)tHead[i].addEventListener("click", ()=>{
+    sortByColumnIndex(i);
+});
+const tBody = document.querySelector("table tbody");
+const rows = [
+    ...tBody.querySelectorAll("tr")
+];
+function sortByColumnIndex(index) {
+    rows.sort((rowA, rowB)=>{
+        const cellA = rowA.cells[index].textContent;
+        const cellB = rowB.cells[index].textContent;
+        return cellA.localeCompare(cellB, undefined, {
+            numeric: true
+        });
+    });
+    tBody.innerHTML = "";
+    rows.forEach((row)=>tBody.append(row));
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
